a9585f681152ac152b23b0ed3d01fc3c5a375488
damienbod <damien_bod@hotmail.com>
2017-03-11 23:24:43 +0100

    Adding a UI client for the service

---
+++ AspNetCoreElmah/AspNetCoreElmahUI/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspNetCoreElmahUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            HttpClient _client = new HttpClient();
+            var response = await _client.GetAsync("http://localhost:37209/api/values");
+            response.EnsureSuccessStatusCode();
+            var responseString = System.Text.Encoding.UTF8.GetString(
+                await response.Content.ReadAsByteArrayAsync()
+            );
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            // throws exception
+            HttpClient _client = new HttpClient();
+            var response = await _client.GetAsync("http://localhost:37209/api/values/1");
+            response.EnsureSuccessStatusCode();
+            var responseString = System.Text.Encoding.UTF8.GetString(
+                await response.Content.ReadAsByteArrayAsync()
+            );
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetCoreElmah/AspNetCoreElmahUI/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Elmah.Io.Extensions.Logging;
+using Elmah.Io.AspNetCore;
+
+namespace AspNetCoreElmahUI
+{
+    public class Startup
+    {
+        private string _elmahAppKey;
+        private string _elmahLogId;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets("AspNetCoreElmahUI-c23d2237a4-eb8832a1-452ac5");
+            }
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _elmahAppKey = Configuration["ElmahAppKey"];
+            _elmahLogId = Configuration["ElmahLogId"];
+
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            loggerFactory.AddElmahIo(_elmahAppKey, new Guid(_elmahLogId));
+            app.UseElmahIo(_elmahAppKey, new Guid(_elmahLogId));
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
